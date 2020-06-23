a4e0ef32ef6df311cbfd0c76251ab814c7e585c8
jstaricco10 <jstaricco@correo.um.edu.uy>
2020-06-23 11:40:28 -0300

    aa

--- huffmancompressor/dehuff.py
+++ huffmancompressor/dehuff.py
@@ -6,6 +6,17 @@ import struct
 from collections import namedtuple
 
 
+def decompress(huff, args):
+    file = open(args.file, 'rb')
+    decodificadoTotal = ''
+
+    newfile = open(args.file[:-4] + "orig", 'wb')
+
+    newfile.close()
+    file.close()
+    pass
+
+
 def main():
     parser = argparse.ArgumentParser(description='Parsea los datos pasados en consola')
     parser.add_argument('file', help='Archivo a descomprimir')
@@ -15,21 +26,22 @@ def main():
     file = open(args.file, 'rb')  # debemos controlar el caso de que el file no sea encontrado\
     # Debemos leer el sym array y armar un dicc de named tuples con cada codigo y su correspondiente simbolo
     mn = file.read(2)
-#    print(mn)
+    #    print(mn)
     sym_arraylen = ord(file.read(1)) + 1
     sym_arraysize = ord(file.read(1))
     filelen = file.read(4)
-#    print(sym_arraylen)
-    huffCode = namedtuple('huffCode', ' symbol code')
+    #    print(sym_arraylen)
+    huffCode = namedtuple('huffCode', ' symbol size code')
     huff = []
     for _ in range(sym_arraylen):
         symbol = file.read(1)
         size = file.read(1)
         size = int.from_bytes(size, byteorder='big')
         code = file.read(4)
-        print(size)
-        print(f'%0{size}d' % (struct.unpack('>I', code)[0],))
-    
+        huff.append(huffCode(symbol, size, f'%0{size}d' % (struct.unpack('>I', code)[0],)))
+    print(huff)
+    decompress(huff, args)
+
 
 if __name__ == '__main__':
     main()
